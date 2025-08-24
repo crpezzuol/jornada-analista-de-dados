3c141874822e4040937192d46f5219e7eee7875a
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-08-23 21:50:07 -0300

    Update dia1_ex004.py
    ex004 - Dissecando uma Variável

--- dia1_ex004.py
+++ dia1_ex004.py
@@ -1,3 +1,11 @@
 # ex004 - Dissecando uma Variável
 
-msg = input('Digite algo: ')
+msg = input('Digite algo: \033[1;32m')
+print(f'\033[mO tipo primitivo deste valor é: {type(msg)}')
+print(f'Só tem espaços? {msg.isspace()}')
+print(f'É um número? {msg.isnumeric()}')
+print(f'É alfabético? {msg.isalpha()}')
+print(f'É alfanumérico? {msg.isalnum()}')
+print(f'Está em maiúsculas? {msg.isupper()}')
+print(f'Está em minúsculas? {msg.islower()}')
+print(f'Esta capitalizada? {msg.istitle()}')
