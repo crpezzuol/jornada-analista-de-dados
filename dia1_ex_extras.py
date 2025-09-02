664caeba2b5928134be731c6725b31c356435d07
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-09-01 22:43:41 -0300

    Update dia1_ex_extras.py
    1️⃣ Dobro, Triplo e Raiz Cúbica
    2️⃣ Conversor de Temperatura (Fahrenheit → Celsius/Kelvin)

--- dia1_ex_extras.py
+++ dia1_ex_extras.py
@@ -2,6 +2,8 @@
 
 # 1️⃣ Dobro, Triplo e Raiz Cúbica
 # Crie um algoritmo que peça um número inteiro e mostre o dobro, o triplo e a raiz cúbica (com duas casas decimais).
+print("\n" + "📌" * 10 + " Exercícios Extras – Fixação Dia 1 🧠🔥 " + "📌" * 10 + "\n")
+print("1️⃣  Dobro, Triplo e Raiz Cúbica:\n")
 num = int(input("Digite um número inteiro: \033[1;32m "))
 dobro = num * 2
 triplo = num * 3
@@ -10,7 +12,14 @@ print(f"\033[m\nO dobro de {num} é {dobro}")
 print(f"O triplo de {num} é {triplo}")
 print(f"A raiz cúbica de {num} é {raiz_cubica:.2f}\n")
 
-# 2️⃣ 
+# 2️⃣ Conversor de Temperatura (Fahrenheit → Celsius/Kelvin)
+# Peça a temperatura em Fahrenheit e mostre a conversão em Celsius e Kelvin.
+print("2️⃣  Conversor de Temperatura (Fahrenheit → Celsius/Kelvin:)\n")
+fah = float(input("Digite a temperatura em Fahrenheit: \033[1;32m "))
+celsius = (fah - 32) * 5/9
+kelvin = celsius + 273.15
+print(f"\033[m\nA temperatura de {fah}°F corresponde a {celsius:.2f}°C")
+print(f"E a temperatura de {fah}°F corresponde a {kelvin:.2f}K\n")
 
 # 3️⃣ 
 
