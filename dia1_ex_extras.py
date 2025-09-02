97ad69452354233621b3cab83489459491075662
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-09-01 22:22:01 -0300

    Create dia1_ex_extras.py
    Exercícios Extras – Fixação Dia 1

---
+++ dia1_ex_extras.py
@@ -0,0 +1,11 @@
+# 📌 Exercícios Extras – Fixação Dia 1 🧠🔥
+
+# 1️⃣ Número par ou ímpar
+
+# 2️⃣ Maior entre 3 números
+
+# 3️⃣ Calculadora simples
+
+# 4️⃣ Tabuada com laço
+
+# 5️⃣ Verificar se número é primo
