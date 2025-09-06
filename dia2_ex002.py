efa88f143c5d501ddcbea34e8d94a163bc19dc89
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-09-06 00:27:55 -0300

    Update dia2_ex002.py
    # ex002 - Radar eletrônico
    # Este programa verifica a velocidade de um carro e calcula a multa se necessário.
    # O limite de velocidade é de 80 km/h e a multa é de R$ 7 por km/h acima do limite.
    # Exemplo de condição simples, sem else.

--- dia2_ex002.py
+++ dia2_ex002.py
@@ -2,3 +2,10 @@
 # Este programa verifica a velocidade de um carro e calcula a multa se necessário.
 # O limite de velocidade é de 80 km/h e a multa é de R$ 7 por km/h acima do limite.
 # Exemplo de condição simples, sem else.
+
+vel = int(input("Qual é a velocidade atual do carro em km/h?: "))
+if vel > 80:
+    multa = (vel - 80) * 7
+    print('Você foi multado! Você excedeu o limite permitido que é de 80km/h.')
+    print(f'Você deve pagar uma multa de R$ {multa:.2f}!')
+print('Tenha cuidado! E dirija com segurança!')
