cc0799ef1f25eb64d67651e0c215d75aee96933b
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-09-09 23:59:46 -0300

    Create dia2_ex012_2.py
    ex012 - Alistamento Militar
    Faça um programa que leia o sexo e o ano de nascimento de um jovem e informe, de acordo com seu sexo e a sua idade, se ele precisa se alistar, se ele ainda vai se alistar ao serviço militar, se é a hora de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.

---
+++ dia2_ex012_2.py
@@ -0,0 +1,5 @@
+# ex012 - Alistamento Militar
+
+'''
+Faça um programa que leia o sexo e o ano de nascimento de um jovem e informe, de acordo com seu sexo e a sua idade, se ele precisa se alistar, se ele ainda vai se alistar ao serviço militar, se é a hora de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+'''
